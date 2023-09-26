a3abc9d1a99ececa6983f62eaaf948487ffb3423
peter-sila1 <138575611+peter-sila1@users.noreply.github.com>
2023-09-26 14:57:56 +0300

    Add files via upload

---
+++ control_flow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+def admin_login(username, password):
+    # your code here
+    if username.upper() == "ADMIN" and password == "1234":
+        return "Access granted"
+    return "Access denied"
+
+def hows_the_weather(temperature):
+    # your code here
+    if temperature < 40:
+          return "Its perfect out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    return "It's perfect out there!"
+
+def fizzbuzz(num):
+    # your code here
+    if not num % 15:
+        return "FizzBuzz"
+    elif not num % 5:
+        return "Buzz"
+    elif not num % 3:
+        return "Fizz"
+    return num
+
+def calculator(operation, num1, num2):
+    # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    print("Invalid operation!")
+    return None
